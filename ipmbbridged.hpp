c5ec5c54be414ff8dc19ea068812b6d9a562a63a
Kumar Thangavel <thangavel.k@hcl.com>
2020-07-10 18:07:33 +0530

    Ipmbbridge: yosemitev2: ipmbbridge to support multiple channels.
    Added Multiple sub channels support in the ipmb.
    Now we are able to communicate with multiple sub channels.
    
    ChannelType supports only two channels ME and ipmb.
    if we want to support multiple sub channels or multiple ME or
    multiple Ipmb, we can use both channelIdx and channelType approach.
    It will not affect the existing functionalities.
    
    Single host, We need not to update any configurations in the
    json. The "channelIdx" will be set as default value.
    
    Multi host, We can provide appropriate channelIdx in the json
    configurations. In sendRequest function, first parameter
    values can set for ipmb channel as 0, 4, 8, 12, etc..
    for ME channel as 1, 5, 9, 13. Because this approach uses the
    first parameter values for MSB 6 bits as "channelIdx" and
    2 bits as "channelType".
    
    In the existing code, ipmb=0 me=1. The current code also uses the
    same. Each channelIdx can have one "me" and "ipmb"
    channel. To ensure existing platforms does not get affected.
    if the "channelIdx" key is not present in the file
    ipmbbridged uses default "channelIdx" as 0.
    
    TESTED : Built Facebook YosemiteV2 & Tiogapass images and loaded on
    the target hardware. We are able to see BMC to all
    host communications through Ipmb.
    
    Signed-off-by: Kumar Thangavel <thangavel.k@hcl.com>
    Change-Id: I20a7231993a0dd1f9d396dde114837380302dc76

--- ipmbbridged.hpp
+++ ipmbbridged.hpp
@@ -262,7 +262,8 @@ class IpmbChannel
 {
   public:
     IpmbChannel(boost::asio::io_service &io, uint8_t ipmbBmcSlaveAddress,
-                uint8_t ipmbRqSlaveAddress, ipmbChannelType type,
+                uint8_t ipmbRqSlaveAddress, uint8_t channelIdx,
+                ipmbChannelType type,
                 std::shared_ptr<IpmbCommandFilter> commandFilter);
 
     IpmbChannel(const IpmbChannel &) = delete;
@@ -277,6 +278,8 @@ class IpmbChannel
     ipmbChannelType getChannelType();
 
     uint8_t getBusId();
+
+    uint8_t getChannelIdx();
 
     uint8_t getBmcSlaveAddress();
 
@@ -301,6 +304,7 @@ class IpmbChannel
     uint8_t ipmbBmcSlaveAddress;
     uint8_t ipmbRqSlaveAddress;
     uint8_t ipmbBusId;
+    uint8_t channelIdx;
 
     ipmbChannelType type;
 
