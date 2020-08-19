72a2d42aadabd6e71b90a334bebb946f21882b38
Kumar Thangavel <thangavel.k@hcl.com>
2020-08-19 21:42:23 +0530

    Ipmbbridge: Add host Identification support for ipmid.
    Added hostId support for ipmid. This helps in
    identifying host and sending host id to ipmid for
    multi host support. Host ids are configured through json config file.
    
    TESTED : Built Facebook YosemiteV2 & Tiogapass images and loaded on
    the target hardware. We are able to see hostId's for all the hosts.
    
    Signed-off-by: Kumar Thangavel <thangavel.k@hcl.com>
    Change-Id: Ie232d61a0e1f69d2a48d472160ba58a1ea64a6d7

--- ipmbbridged.cpp
+++ ipmbbridged.cpp
@@ -429,8 +429,12 @@ void IpmbChannel::processI2cEvent()
         auto ipmbMessageReceived = IpmbRequest();
         ipmbMessageReceived.i2cToIpmbConstruct(ipmbFrame, r);
 
+        int devId = getDevIndex();
+
         std::map<std::string, std::variant<int>> options{
-            {"rqSA", ipmbAddressTo7BitSet(ipmbMessageReceived.rqSA)}};
+            {"rqSA", ipmbAddressTo7BitSet(ipmbMessageReceived.rqSA)},
+            {"channelIdx", devId}};
+
         using IpmiDbusRspType = std::tuple<uint8_t, uint8_t, uint8_t, uint8_t,
                                            std::vector<uint8_t>>;
         conn->async_method_call(
